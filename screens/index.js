69890619f452e98c30c27669f6f0e715a587d6e9
Thomas-Max99 <thomassentre332@gmail.com>
2023-04-21 19:23:09 +0000

    first commit

---
+++ screens/index.js
@@ -0,0 +1,19 @@
+import GetStarted from "./GetStarted";
+import Home from "./Home";
+import Login from "./Login";
+import OnboardingStarter from "./OnboardingStarter";
+import OTPVerification from "./OTPVerification";
+import Register from "./Register";
+import ResetPassword from "./ResetPassword";
+import SuccessVerification from "./SuccessVerification";
+
+export {
+    GetStarted,
+    Home,
+    Login,
+    OnboardingStarter,
+    OTPVerification,
+    Register,
+    ResetPassword,
+    SuccessVerification
+}
