d8a5a4afd47637a969d1d7edaea659788f30bf9a
rc0fficial <to.arsalanali@gmail.com>
2023-09-12 02:26:06 +0500

    update on code

--- App.js
+++ App.js
@@ -66,6 +66,7 @@ export default function App() {
 
             >
                 
+
                 <Stack.Screen
                     name="OnboardingStarter"
                     component={OnboardingStarter}
