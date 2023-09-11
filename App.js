39556620a03b17ed49b8d5f984ce3e313de4153e
rc0fficial <to.arsalanali@gmail.com>
2023-09-12 02:25:53 +0500

    update on code

--- App.js
+++ App.js
@@ -70,8 +70,9 @@ export default function App() {
                     component={OnboardingStarter}
                     options={{
                         headerShown: false,
-
                         
+
+
                     }}
                 />
                 <Stack.Screen
