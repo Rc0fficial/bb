f11d9f42159b2482f1d9da89d8bafb1887d433b9
rc0fficial <to.arsalanali@gmail.com>
2023-09-17 02:29:55 +0500

    update on code

--- App.js
+++ App.js
@@ -77,6 +77,7 @@ export default function App() {
                     }}
                 />
                 <Stack.Screen
+                
                     name="BottomTabNavigation"
                     component={BottomTabNavigation}
                     options={{
