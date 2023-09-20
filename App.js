64c174457016535c502a2dadc0736d9f30e4dcd3
rc0fficial <to.arsalanali@gmail.com>
2023-09-21 02:31:13 +0500

    update on code

--- App.js
+++ App.js
@@ -72,6 +72,7 @@ export default function App() {
                     component={OnboardingStarter}
                     options={{
                         headerShown: false,
+                        
 
 
                     }}
